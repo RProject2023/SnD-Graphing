0c7cc01198c4b03571951adff00c7e40e8c88d3a
Lorena <lazar.lorena96@gmail.com>
2024-01-07 11:52:50 +0100

    added notebook

--- supply-demand-linear.R
+++ supply-demand-linear.R
@@ -1,26 +1,149 @@
+library(dplyr)
 library(ggplot2)
-library(patchwork)
+library(Hmisc)
 
-linear_curve_2_ponts <- function(x1, y1, x2, y2, title, color="black", minX=0, maxX=10, minY=0, maxY=10) {
-  x_axis <- c(x1, x2)
-  y_axis <- c(y1, y2)
-  points <- data.frame(x_axis, y_axis)
-  slope <- (y2 - y1) / (x2 - x1)
-  intercept <- y1 - slope * x1
+linear_curve_2_ponts <- function(...,
+                                 title,
+                                 color="black",
+                                 minX=0,
+                                 maxX=10,
+                                 minY=0,
+                                 maxY=10,
+                                 xlabel="Q - quantity",
+                                 ylabel="P - price",
+                                 pointsHidden=TRUE,
+                                 lineCoordinateHidden=TRUE) {
   
-  gg <- ggplot(points, aes(x_axis, y_axis)) +
-    geom_point(color = color, size = 3) +
-    geom_abline(intercept = intercept, slope = slope, color = color) +
-    geom_segment(aes(x = x1, y = y1, xend = x1, yend = minY), linetype = "dashed", color = "black") +
-    geom_segment(aes(x = x2, y = y2, xend = x2, yend = minY), linetype = "dashed", color = "black") +
-    geom_segment(aes(x = x1, y = y1, xend = minX, yend = y1), linetype = "dashed", color = "black") +
-    geom_segment(aes(x = x2, y = y2, xend = minX, yend = y2), linetype = "dashed", color = "black") +
-    xlim(minX, maxX) +
-    ylim(minY, maxY) +
-    labs(title = title, x = "Q - quantity", y = "P - price")
+  gg <- ggplot(data.frame(x = c(minX, maxX), y = c(minY, minY)), aes(x = x, y = y))
+  points_df <- data.frame()
+  segments_df <- data.frame()
+  
+  for(i in seq_along(list(...))) {
+    points <- unlist(list(...)[[i]])
+    
+    x1 <- points[1]
+    y1 <- points[2]
+    x2 <- points[3]
+    y2 <- points[4]
+    
+    x_axis <- c(x1, x2)
+    y_axis <- c(y1, y2)
+    points_df <- rbind(points_df, data.frame(x = x_axis, y = y_axis))
+    segments_df <- rbind(segments_df, data.frame(x = c(x1, x1, minX), y = c(y1, minY, y1), xend = x1, yend = y1))
+    segments_df <- rbind(segments_df, data.frame(x = c(x2, x2, minX), y = c(y2, minY, y2), xend = x2, yend = y2))
+    
+    slope <- (y2 - y1) / (x2 - x1)
+    intercept <- y1 - slope * x1
+    
+    gg <- gg + geom_abline(intercept = intercept, slope = slope, color = color)
+  }
+  
+  if (!pointsHidden) {
+    gg <- gg + geom_point(data = points_df, aes(x = x, y = y), color = "black", size = 3)    
+  }
+  
+  if (!lineCoordinateHidden) {
+    gg <- gg + geom_segment(data = segments_df, aes(x = x, y = y, xend = xend, yend = yend), linetype = "dashed", color = "black")
+  }
+  
+  gg <- gg +
+    xlim(minX, maxX) + 
+    ylim(minY, maxY) + 
+    labs(title = title, x = xlabel, y = ylabel) +
+    theme_classic()
   
   return(gg)
 }
 
-linear_curve_2_ponts(1, 2, 5, 7, "Supply", "blue") + supply(3, 10, 6, 7, "Demand", "red")
 
+linear_curve_2_characteristic <- function(...,
+                                          title,
+                                          color="black",
+                                          minX=0,
+                                          maxX=10,
+                                          minY=0,
+                                          maxY=10,
+                                          xlabel="Q - quantity",
+                                          ylabel="P - price") {
+  
+  gg <- ggplot(data.frame(x = c(minX, maxX), y = c(minY, minY)), aes(x = x, y = y))
+  points_df <- data.frame()
+  segments_df <- data.frame()
+  
+  for(i in seq_along(list(...))) {
+    characteristic <- unlist(list(...)[[i]])
+    
+    slope <- characteristic[1]
+    intercept <- characteristic[2]
+    
+    gg <- gg + geom_abline(intercept = intercept, slope = slope, color = color)
+  }
+  
+  gg <- gg +
+    xlim(minX, maxX) + 
+    ylim(minY, maxY) + 
+    labs(title = title, x = xlabel, y = ylabel) +
+    theme_classic()
+  
+  return(gg)
+}
+
+find_intersection <- function(p1, p2, gg) {
+  slope1 <- (p1[4] - p1[2]) / (p1[3] - p1[1])
+  intercept1 <- p1[2] - slope1 * p1[1]
+  
+  slope2 <- (p2[4] - p2[2]) / (p2[3] - p2[1])
+  intercept2 <- p2[2] - slope2 * p2[1]
+  
+  x <- (intercept2 - intercept1) / (slope1 - slope2)
+  y <- slope1 * x + intercept1
+  
+  points_df <- data.frame()
+  segments_df <- data.frame()
+  points_df <- rbind(points_df, data.frame(x = x, y = y))
+  segments_df <- rbind(segments_df, data.frame(x = c(x, x, 0), y = c(y, 0, y), xend = x, yend = y))
+  
+  gg <- gg +
+    geom_point(data = points_df, aes(x = x, y = y), color = "black", size = 3) +
+    geom_segment(data = segments_df, aes(x = x, y = y, xend = xend, yend = yend), linetype = "dashed", color = "black")
+  
+  
+  return (gg)
+}
+
+curve_N_ponts <- function(...,
+                          title,
+                          xlabel="Q - quantity",
+                          ylabel="P - price") {
+  gg <- ggplot()
+  
+  for (i in seq_along(list(...))) {
+    curve_data <- data.frame(list(...)[[i]])
+    gg <- gg + geom_path(data = curve_data, aes(x = x, y = y), linewidth = 1)
+  }
+  
+  gg <- gg +
+    labs(x = xlabel, y = ylabel, title = title) +
+    theme_classic() +
+    coord_equal()
+  
+  return(gg)
+}
+
+curve_intersect <- function(curve1, curve2, gg) {
+  curve1_f <- approxfun(curve1$x, curve1$y, rule = 2)
+  curve2_f <- approxfun(curve2$x, curve2$y, rule = 2)
+  
+  point_x <- uniroot(function(x) curve1_f(x) - curve2_f(x),
+                     c(min(curve1$x), max(curve1$x)))$root
+  
+  point_y <- curve2_f(point_x)
+  intersections <- bind_rows(list(x = point_x, y = point_y))
+  
+  gg <- gg +
+    geom_segment(data = intersections, aes(x = point_x, y = 0, xend = point_x, yend = point_y), lty = "dotted") +
+    geom_segment(data = intersections, aes(x = point_x, y = point_y, xend = 0, yend = point_y), lty = "dotted") +
+    geom_point(data = intersections, size = 3)
+  
+  return(gg)
+}
